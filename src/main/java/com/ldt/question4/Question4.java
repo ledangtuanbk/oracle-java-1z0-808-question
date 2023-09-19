e2ee590e66eb889740e15f6f1aa57a06bc42765c
tuanld <ledangtuanbk@gmail.com>
2023-09-19 15:44:21 +0700

    add question 4

---
+++ src/main/java/com/ldt/question4/Question4.java
@@ -0,0 +1,29 @@
+//package com.udayan.oca;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        /*INSERT*/
+//        arr[1] = 5;
+//        arr[2] = 10;
+//        System.out.println("[" + arr[1] + ", " + arr[2] + "]"); //Line n1
+//    }
+//}
+
+
+//        1. short arr [] = new short[2];
+//
+//        2. byte [] arr = new byte[10];
+//
+//        3. short [] arr; arr = new short[3];
+//
+//        4. short [2] arr;
+//
+//        5. short [3] arr;
+//
+//        6. int [] arr = new int[]{100, 100};
+//
+//        7. int [] arr = new int[]{0, 0, 0, 0};
+//
+//        8. short [] arr = {};
+//
+//        9. short [] arr = new short[2]{5, 10};
