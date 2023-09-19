e8fc9cb01b182ec41c25c683888133f7216d406a
tuanld <ledangtuanbk@gmail.com>
2023-09-19 15:04:24 +0700

    Update question 3

---
+++ src/main/java/com/ldt/question3/Question3.java
@@ -0,0 +1,17 @@
+//package com.udayan.oca;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Test {
+//    public static void main(String [] args) {
+//        List<Integer> list = new ArrayList<Integer>();
+//        list.add(new Integer(2));
+//        list.add(new Integer(1));
+//        list.add(new Integer(0));
+//
+//        list.remove(list.indexOf(0));
+//
+//        System.out.println(list);
+//    }
+//}
