3abdb514243a233d16fcec5cc1fb9aa50e83cd89
tuanld <ledangtuanbk@gmail.com>
2023-09-19 14:20:45 +0700

    Update question 2

---
+++ src/main/java/com/ldt/question2/Question2.java
@@ -0,0 +1,20 @@
+//package com.udayan.oca;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        String[] names = { "Smith", "Brown", "Thomas", "Taylor", "Jones" };
+//        List<String> list = new ArrayList<>();
+//        for (int x = 0; x < names.length; x++) {
+//            list.add(names[x]);
+//            switch (x) {
+//                case 2:
+//                    continue;
+//            }
+//            break;
+//        }
+//        System.out.println(list.size());
+//    }
+//}
