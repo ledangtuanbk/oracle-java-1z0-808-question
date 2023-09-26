7066102e6a5853ce435801104303421f712cb1ae
tuanld <ledangtuanbk@gmail.com>
2023-09-27 06:58:16 +0700

    add question 10

---
+++ src/main/java/com/ldt/question10/Test.java
@@ -0,0 +1,18 @@
+//package com.udayan.oca;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        String fruit = "mango";
+//        switch (fruit) {
+//            default:
+//                System.out.println("ANY FRUIT WILL DO");
+//            case "Apple":
+//                System.out.println("APPLE");
+//            case "Mango":
+//                System.out.println("MANGO");
+//            case "Banana":
+//                System.out.println("BANANA");
+//                break;
+//        }
+//    }
+//}
