75e720a624d94dd65aeebd64fb879e077116c6b3
tuanld <ledangtuanbk@gmail.com>
2023-09-27 09:59:53 +0700

    add question 11

---
+++ src/main/java/com/ldt/question11/Test.java
@@ -0,0 +1,17 @@
+//package com.udayan.oca;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        String s = new String("Hello");
+//        List<String> list = new ArrayList<>();
+//        list.add(s);
+//        list.add(new String("Hello"));
+//        list.add(s);
+//        s.replace("l", "L");
+//
+//        System.out.println(list);
+//    }
+//}
