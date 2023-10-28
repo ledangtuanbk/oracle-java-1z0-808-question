2f6b9d672e27a81e6422a46cb70598c3cd5ed8f3
tuanld <ledangtuanbk@gmail.com>
2023-10-28 15:34:11 +0700

    add question 15

---
+++ src/main/java/com/ldt/question15/Test.java
@@ -0,0 +1,12 @@
+//package com.udayan.oca;
+
+//import java.time.LocalDate;
+//import java.time.LocalTime;
+//
+//public class Test {
+//    public static void main(String [] args) {
+//        LocalDate date = LocalDate.parse("1947-08-14");
+//        LocalTime time = LocalTime.MAX;
+//        System.out.println(date.atTime(time));
+//    }
+//}
